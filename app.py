7dcc389a54e7a497ceb338ab68e7549fab893d01
abdulkadirdmrc <demrcabdulkadir@gmail.com>
2023-01-15 19:58:15 +0300

    sadf

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import requests
 import streamlit as st
-from sklearn.neighbors import LocalOutlierFactor
 from streamlit_lottie import st_lottie
 from PIL import Image
 pd.set_option("display.expand_frame_repr",False)
